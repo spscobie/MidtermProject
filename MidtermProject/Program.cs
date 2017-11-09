0405de437e05c507770637c5bb79e1a5b30e6d3e
SheffieldPublicWorks <stephen.scobie@gmail.com>
2017-11-09 14:27:00 -0500

    Created inventory display method in ShoppingCart.

--- MidtermProject/Program.cs
+++ MidtermProject/Program.cs
@@ -10,273 +10,108 @@ namespace MidtermProject
 {
     class Program
     {
-        const string FILENAME = "inventory.txt";
+        //const string FILENAME = "inventory.txt";
 
         static void Main(string[] args)
         {
-            StreamReader inventory = new StreamReader(FILENAME);
-            ArrayList menu = new ArrayList();
+        //    StreamReader inventory = new StreamReader(FILENAME);
+        //    ArrayList menu = new ArrayList();
 
-<<<<<<< HEAD
-            Console.WriteLine("                   Welcome to our delivery food service! ");
-            DisplayInventory(inventory, menu);
-=======
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            /************* Scobie **************/
+        //    Console.WriteLine("                   Welcome to our delivery food service! ");
+        //    DisplayInventory(inventory, menu);
 
->>>>>>> a64318c9ec5cb9225be57b3982edf9a669b388f1
+        //    Console.Write("What would you like to pick? ");
+        //    string input = Console.ReadLine().ToLower().Trim();
+        //    //enter in validation for input to match for numbers
+        //    //enter in validation for categories
 
-            Console.Write("What would you like to pick? ");
-            string input = Console.ReadLine().ToLower().Trim();
-            //enter in validation for input to match for numbers
-            //enter in validation for categories
-
-            int.TryParse(input, out int choice);
+        //    int.TryParse(input, out int choice);
 
 
 
 
-            Console.WriteLine();
-            Console.WriteLine(menu[choice]);
-            Console.WriteLine();
+        //    Console.WriteLine();
+        //    Console.WriteLine(menu[choice]);
+        //    Console.WriteLine();
 
 
-            bool repeat = true;
-            while (repeat)
-            {
+        //    bool repeat = true;
+        //    while (repeat)
+        //    {
 
-                Console.WriteLine(Validator.GetValidSelection());
-                Console.WriteLine("Would you like to add this item to your shopping cart? (Y/N): ");
-                bool addCart = Validator.YesNo();
-                Console.WriteLine($"You added {0} ${0}to your shopping cart");
+        //        Console.WriteLine(Validator.GetValidSelection());
+        //        Console.WriteLine("Would you like to add this item to your shopping cart? (Y/N): ");
+        //        bool addCart = Validator.YesNo();
+        //        Console.WriteLine($"You added {0} ${0}to your shopping cart");
 
-                Console.WriteLine("Would you like to continue shopping? (Y/N)");
-                bool shopAgain = Validator.YesNo();
+        //        Console.WriteLine("Would you like to continue shopping? (Y/N)");
+        //        bool shopAgain = Validator.YesNo();
 
-                Console.WriteLine("Would you like to checkout? (Y/N)");
-                bool checkOut = Validator.YesNo();
+        //        Console.WriteLine("Would you like to checkout? (Y/N)");
+        //        bool checkOut = Validator.YesNo();
 
-                Console.WriteLine(Validator.Mod10Check());
-            }
+        //        Console.WriteLine(Validator.Mod10Check());
+        //    }
 
-        }
+        //}
 
-        private static void DisplayInventory(StreamReader inventory, ArrayList menu)
-        {
+        //private static void DisplayInventory(StreamReader inventory, ArrayList menu)
+        //{
+        //    while (true)
+        //    {
+
+        //        string line = inventory.ReadLine();
+        //        if (string.IsNullOrEmpty(line))
+        //        {
+        //            break;
+        //        }
+
+        //        string[] itemParts = line.Split('\t');
+        //        string name = itemParts[0];
+        //        string category = itemParts[1];
+        //        string description = itemParts[2];
+        //        double.TryParse(itemParts[3], out double price);
+        //        int.TryParse(itemParts[4], out int quantity);
+
+        //        Product menuItem = new Product(name, category, description, price, quantity);
+        //        quantity = menuItem.Quantity;
+        //        menu.Add(menuItem);
+
+        //    }
+        //    inventory.Close();
+        //    Console.WriteLine("             Here is what we have in stock right now! ");
+        //    Console.WriteLine("==========================================================================");
+        //    for (int i = 0; i < menu.Count; i++)
+        //    {
+        //        Console.WriteLine($"{i}:{menu[i]}");
+        //    }
+        //    Console.WriteLine("==========================================================================");
+
+
+            /****** Scobie ******/
+            ShoppingCart cart = new ShoppingCart();
             while (true)
             {
+                ArrayList CurrentInventory = cart.LoadInventory();
+                ShoppingCart.ListInventory(CurrentInventory);
+                
 
-                string line = inventory.ReadLine();
-                if (string.IsNullOrEmpty(line))
+                Console.Write("Do you want to continue? (y/n): ");
+                string userInput = Console.ReadLine();
+
+                if (int.TryParse(userInput, out int junk))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("I'm sorry, but that was not a valid option. Please try again");
+                    Console.WriteLine();
+                    break;
+                }
+                else if (userInput.ToLower() != "y")
                 {
                     break;
                 }
-
-                string[] itemParts = line.Split('\t');
-                string name = itemParts[0];
-                string category = itemParts[1];
-                string description = itemParts[2];
-                double.TryParse(itemParts[3], out double price);
-                int.TryParse(itemParts[4], out int quantity);
-
-                Product menuItem = new Product(name, category, description, price, quantity);
-                quantity = menuItem.Quantity;
-                menu.Add(menuItem);
-
+               
             }
-            inventory.Close();
-            Console.WriteLine("             Here is what we have in stock right now! ");
-            Console.WriteLine("==========================================================================");
-            for (int i = 0; i < menu.Count; i++)
-            {
-                Console.WriteLine($"{i}:{menu[i]}");
-            }
-            Console.WriteLine("==========================================================================");
         }
     }
 }
--- MidtermProject/ShoppingCart.cs
+++ MidtermProject/ShoppingCart.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,10 @@ namespace MidtermProject
 {
     class ShoppingCart
     {
+        private const string INVENTORY = "Inventory.txt";
+        StreamReader fileIn;
+        StreamWriter fileOut;
+
         private long? transId;
         public long? TransId
         {
@@ -30,10 +35,75 @@ namespace MidtermProject
             Console.WriteLine("Inside ShoppingCart.GenerateCust()");
         }
 
-        public void QueryInventory ()
+        public static void ListInventory(ArrayList inv)
         {
-            StreamReader fileIn = new StreamReader("Inventory.txt");
-            Console.WriteLine("Inside ShoppingCart.QueryInventory()");
+            if (inv != null)
+            {
+                string name;
+                string category;
+                string description;
+                double price;
+                int qty;
+
+                Console.WriteLine(new string('+', 110)); //header
+                for (int item = 0; item < inv.Count; item++)
+                {
+                    string[] itemInfo = ((string)inv[item]).Split('\t');
+
+                    /************ Set attributes for display ****************/
+                    name = itemInfo[0].Trim();
+                    category = itemInfo[1].Trim();
+                    description = itemInfo[2].Trim();
+                    double.TryParse(itemInfo[3].Trim(), out price);
+                    int.TryParse(itemInfo[4], out qty);
+                    /********************************************************/
+
+                    Console.WriteLine($"| {item + 1, -4} | {name, -20} | {category, -15} | {description, -30} | {price, 10:C} | {qty, 7:n0} |");
+                }
+
+                Console.WriteLine(new string('+', 110)); //footer
+                Console.WriteLine();
+            }
+        }
+
+        public ArrayList LoadInventory ()
+        {
+            fileIn = new StreamReader("Inventory.txt");
+
+            try
+            {
+                fileIn = new StreamReader(INVENTORY);
+            }
+            catch (SystemException e)
+            {
+                Console.WriteLine();
+                Console.WriteLine("ERROR READING FILE: Country DB does not exist.");
+                Console.WriteLine("Add a country (option #2) and the database will be created for you.");
+                Console.WriteLine($"DETAILS: {e.Message}");
+                return null;
+            }
+
+            ArrayList arrLst = new ArrayList();
+            if (fileIn.EndOfStream)
+            {
+                Console.WriteLine("End of file reached. No records exist in the database! See the manager.");
+                return null;
+            }
+            else
+            {
+                string input;
+                while (fileIn.EndOfStream == false)
+                {
+                    input = fileIn.ReadLine();
+                    if (input != "")
+                    {
+                        arrLst.Add(input.Trim());
+                    }
+                }
+
+                fileIn.Close();
+                return arrLst;
+            }
         }
 
         public void RemoveFromCart()
